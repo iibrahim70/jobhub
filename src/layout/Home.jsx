6cfb165d5b1303d745925866a6d7d969c1517147
iibrahim70 <iibrahiim.dev@gmail.com>
2023-12-16 11:13:51 +0600

    added lenis

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,13 +1,26 @@
-import Category from "../components/Category";
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import LatestJobs from "../components/LatestJobs";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 import Opportunities from "../components/Opportunities";
+import Category from "../components/Category";
+import LatestJobs from "../components/LatestJobs";
 import Promotional from "../components/Promotional";
 import Review from "../components/Review";
+import Footer from "../components/Footer";
 
 const Home = () => {
+  const lenis = new Lenis();
+
+  useEffect(() => {
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main>
       <Navbar />
